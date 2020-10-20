c45ff4625b2e949116f7a3bd1bbdde6038d9067a
konifer44 <konifer44@gmail.com>
2020-10-20 17:49:48 +0200

    Initial Commit

---
+++ FacesDetection/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FacesDetection
+//
+//  Created by Jan Konieczny on 20/10/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FacesDetection/FacesDetectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  FacesDetectionApp.swift
+//  FacesDetection
+//
+//  Created by Jan Konieczny on 20/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct FacesDetectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
